c6fe498545a591a0bdedc2be080345f51961adbb
neogul_02 <wlsgud2414@naver.com>
2023-11-09 00:12:06 +0900

    phpFile 추가

---
+++ kproject_api/get_menus.php
@@ -0,0 +1,28 @@
+<?php
+$host = 'localhost';
+$db   = 'kproject';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$connection = new mysqli($host, $user, $pass, $db);
+
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+}
+
+// 메뉴 데이터 가져오기 (GET 요청)
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $result = $connection->query("SELECT * FROM `menu` ORDER BY `kind` ASC");
+    $menus = array();
+
+    while($row = $result->fetch_assoc()) {
+        $menus[] = $row;
+    }
+
+    header("Access-Control-Allow-Origin: *");
+    header('Content-Type: application/json');
+    echo json_encode($menus);
+} 
+$connection->close();
+?>
